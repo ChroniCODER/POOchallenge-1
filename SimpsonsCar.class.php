aa2fe552d3d7e7a178a97d6634b9751fe669e68c
ChroniCODER <thomas.leherisse@gmail.com>
2022-11-05 12:03:14 +0100

    commit du premier challenge POO

---
+++ SimpsonsCar.class.php
@@ -0,0 +1,98 @@
+
+<?php
+class SimpsonsCar{
+
+	private int $nbWheels;
+	private int $currentSpeed;
+	private string $color;
+	private int $nbSeats;
+	private string $energy;
+	private int $energyLevel;
+	
+	//constructor
+
+public function __construct(string $color, INT $nbSeats, string $energy){
+	$this->color = $color;
+	$this->nbSeats = $nbSeats;
+	$this->energy = $energy;
+}
+	//methods
+public function start(): string{
+	$this->currentSpeed = 0;
+	$screen = '<mark>' . "On demarre" . '</mark>';
+	return $screen . "<br>";
+	
+}
+
+public function forward(): string{
+		
+	$this->currentSpeed = 100 ;
+	return '<mark>' . "On accelere à " . $this->currentSpeed . "Km/h" . '</mark>' . "<br>";
+	
+}
+
+public function brake(): string{
+	$show = " ";
+	while ($this->currentSpeed > 0) {
+		$this->currentSpeed--;
+		$show .= '<mark>' . "oula on ralentit !" . '</mark>' . "<br>";
+		
+		}
+		$show .= '<mark>' . "vehicule à l'arret !" . '</mark>';
+		return $show;
+}
+
+public function getNbWheels(): INT{
+	return $this->nbWheels;
+}
+
+public function getCurrentSpeed(){
+	return $this->currentSpeed;
+}
+
+public function getColor(){
+	return $this->color;
+}
+
+public function getNbSeats(){
+	return $this->nbSeats;
+}
+
+public function getEnergy(){
+	return $this->energy;
+}
+
+public function getEnergyLevel(){
+	return $this->energyLevel;
+}
+
+		//SETTERS
+
+public function setNbWheels(INT $new_nbWheels){
+	$this->nbWheels = $new_nbWheels;
+}
+public function setCurrentSpeed(INT $new_currentSpeed){
+	$this->currentSpeed = $new_currentSpeed;
+}
+public function setColor(STRING $new_color){
+	$this->color = $new_color;
+}
+public function setNbSeats(INT $new_nbSeats){
+	$this->nbSeats = $new_nbSeats;
+}
+
+public function setEnergy(STRING $new_energy){
+	$this->energy = $new_energy;
+}
+
+public function setEnergyLevel(INT $new_energyLevel){
+	$this->energyLevel = $new_energyLevel;
+}
+
+
+/* public function dump()
+{
+	var_dump($this);
+} */
+}
+?>
---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Challenge POO donkeyschool Thomas LEHERISSE</title>
+</head>
+<body>
+
+<h1>test de la voiture d'homer</h1>
+
+<?php
+	require 'SimpsonsCar.class.php';
+
+	$homerCar = new SimpsonsCar('pink', 5, 'oil');
+	$homerCar->setNbWheels(4);
+	$homerCar->setCurrentSpeed(0);
+
+	echo $homerCar->start();
+	echo "<br>";
+	var_dump($homerCar);
+	echo "<br>";
+	echo $homerCar->forward();
+	echo "<br>";
+	var_dump($homerCar);
+	echo "<br>";
+	echo $homerCar->brake();
+	echo "<br>";
+	var_dump($homerCar);
+	
+?>
+
+<br><br>
+<h1> Test du velo de bart</h1>
+
+<?php
+	require 'SimpsonsBike.class.php';
+
+	$BartBike = new SimpsonsBike('red', 1, 'manual');
+	$BartBike->setNbWheels(2);
+	$BartBike->setCurrentSpeed(0);
+
+	echo $BartBike->start();
+	echo "<br>";
+	var_dump($BartBike);
+	echo "<br>";
+	echo $BartBike->forward();
+	echo "<br>";
+	var_dump($BartBike);
+	echo "<br>";
+	echo $BartBike->brake();
+	echo "<br>";
+	var_dump($BartBike);
+	
+?>
+<br><br>
+
+<h1>test de la voiture de krusty</h1>
+
+<?php
+	
+	$krustyCar = new SimpsonsCar('grey', 2, 'oil');
+	$krustyCar->setNbWheels(4);
+	$krustyCar->setCurrentSpeed(0);
+
+	echo $krustyCar->start();
+	echo "<br>";
+	var_dump($krustyCar);
+	echo "<br>";
+	echo $krustyCar->forward();
+	echo "<br>";
+	var_dump($krustyCar);
+	echo "<br>";
+	echo $krustyCar->brake();
+	echo "<br>";
+	var_dump($krustyCar);
+	
+?>
+
+<?php
+
+
+?>
+	
+</body>
+</html>
